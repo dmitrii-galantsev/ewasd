8d4bbdceafefaa5e222ff70c36338e203a2d99f7
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2026-04-28 18:01:50 -0500

    Bump flake.nix version to 0.7.0

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
       mkEwasd = pkgs: pkgs.python3Packages.buildPythonApplication {
         pname = "ewasd";
-        version = "0.5.0";
+        version = "0.7.0";
         pyproject = true;
 
         src = ./.;
